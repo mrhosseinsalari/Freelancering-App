2a4925d58baaa8323d0aa3042288820430694d49
Hossein Salari <hosein.salari81@gmail.com>
2024-04-07 13:54:18 +0330

    Create RadioInput component

--- src/features/authentication/CompleteProfileForm.jsx
+++ src/features/authentication/CompleteProfileForm.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import TextField from "../../ui/TextField";
+import RadioInput from "../../ui/RadioInput";
 
 function CompleteProfileForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="w-full max-w-md">
-      <form className="space-y-8">
+      <form className="space-y-8" onSubmit={handleSubmit}>
         <TextField
           label="نام و نام خانوادگی"
           name="name"
@@ -21,26 +27,20 @@ function CompleteProfileForm() {
           onChange={(e) => setEmail(e.target.value)}
         />
         <div className="flex items-center justify-center gap-x-8">
-          <div className="flex items-center gap-x-2 text-secondary-600">
-            <input
-              className="cursor-pointer w-4 h-4 form-radio text-primary-900 focus:ring-primary-900"
-              type="radio"
-              name="role"
-              id="OWNER"
-              value="OWNER"
-            />
-            <label htmlFor="OWNER">کارفرما</label>
-          </div>
-          <div className="flex items-center gap-x-2 text-secondary-600">
-            <input
-              className="cursor-pointer w-4 h-4 form-radio text-primary-900 focus:ring-primary-900"
-              type="radio"
-              name="role"
-              id="FREELANCER"
-              value="FREELANCER"
-            />
-            <label htmlFor="FREELANCER">فریلنسر</label>
-          </div>
+          <RadioInput
+            label="کارفرما"
+            name="role"
+            value="OWNER"
+            onChange={(e) => setRole(e.target.value)}
+            checked={role === "OWNER"}
+          />
+          <RadioInput
+            label="فریلنسر"
+            name="role"
+            value="FREELANCER"
+            onChange={(e) => setRole(e.target.value)}
+            checked={role === "FREELANCER"}
+          />
         </div>
         <button className="btn btn--primary w-full">تایید</button>
       </form>
