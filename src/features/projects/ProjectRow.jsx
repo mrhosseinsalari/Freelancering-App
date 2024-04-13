af4c8203a9b6aa8d8d251ff41a391601bda2380c
Hossein Salari <hosein.salari81@gmail.com>
2024-04-13 20:44:37 +0330

    create ConfirmDelete component

--- src/features/projects/ProjectRow.jsx
+++ src/features/projects/ProjectRow.jsx
@@ -6,9 +6,11 @@ import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
 import truncateText from "../../utils/truncateText";
 import { HiOutlineTrash } from "react-icons/hi";
 import { TbPencilMinus } from "react-icons/tb";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 function ProjectRow({ project, index }) {
   const [isEditOpen, setIsEditOpen] = useState(false);
+  const [isDeleteOpen, setIsDeleteOpen] = useState(false);
 
   return (
     <Table.Row>
@@ -36,19 +38,35 @@ function ProjectRow({ project, index }) {
       </td>
       <td>
         <div className="flex items-center gap-x-4">
-          <button onClick={() => setIsEditOpen(true)}>
-            <TbPencilMinus className="w-5 h-5 text-primary-900" />
-          </button>
-          <Modal
-            title="modal title"
-            open={isEditOpen}
-            onClose={() => setIsEditOpen(false)}
-          >
-            This is modal
-          </Modal>
-          <button>
-            <HiOutlineTrash className="w-5 h-5 text-error" />
-          </button>
+          <>
+            <button onClick={() => setIsEditOpen(true)}>
+              <TbPencilMinus className="w-5 h-5 text-primary-900" />
+            </button>
+            <Modal
+              title={`ویرایش ${project.title}`}
+              open={isEditOpen}
+              onClose={() => setIsEditOpen(false)}
+            >
+              This is modal
+            </Modal>
+          </>
+          <>
+            <button onClick={() => setIsDeleteOpen(true)}>
+              <HiOutlineTrash className="w-5 h-5 text-error" />
+            </button>
+            <Modal
+              title={`حذف ${project.title}`}
+              open={isDeleteOpen}
+              onClose={() => setIsDeleteOpen(false)}
+            >
+              <ConfirmDelete
+                resourceName={project.title}
+                onClose={() => setIsDeleteOpen(false)}
+                onConfirm={() => {}}
+                disabled={false}
+              />
+            </Modal>
+          </>
         </div>
       </td>
     </Table.Row>
---
+++ src/ui/ConfirmDelete.jsx
@@ -0,0 +1,27 @@
+function ConfirmDelete({ resourceName, onClose, disabled, onConfirm }) {
+  return (
+    <div>
+      <h2 className="font-bold text-base mb-8">
+        آیا از حذف {resourceName} مطمئن هستید؟
+      </h2>
+      <div className="flex items-center justify-between gap-x-16">
+        <button
+          className="btn btn--primary flex-1"
+          onClick={onClose}
+          disabled={disabled}
+        >
+          لغو
+        </button>
+        <button
+          className="btn btn--danger flex-1 py-3"
+          onClick={onConfirm}
+          disabled={disabled}
+        >
+          تایید
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ConfirmDelete;
