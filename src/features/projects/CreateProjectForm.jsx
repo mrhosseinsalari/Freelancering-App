db86d27a01a90b4ba007bf6a4d25783e1ed8bfca
Hossein Salari <hosein.salari81@gmail.com>
2024-04-17 17:22:25 +0330

    implement TextField with React Hook Form

---
+++ src/features/projects/CreateProjectForm.jsx
@@ -0,0 +1,38 @@
+import TextField from "../../ui/TextField";
+import { useForm } from "react-hook-form";
+
+function CreateProjectForm() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
+      <TextField
+        label="عنوان پروژه"
+        name="title"
+        register={register}
+        required
+        validationSchema={{
+          required: "عنوان ضروری است",
+          minLength: {
+            value: 10,
+            message: "طول عنوان نامعتبر است",
+          },
+        }}
+        errors={errors}
+      />
+      <button type="submit" className="btn btn--primary w-full">
+        تایید
+      </button>
+    </form>
+  );
+}
+
+export default CreateProjectForm;
