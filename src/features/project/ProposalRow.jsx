4072b9548a3bc088eb7b902727e6ee4e13981af9
Hossein Salari <hosein.salari81@gmail.com>
2024-04-19 21:55:14 +0330

    implement change proposal status

--- src/features/project/ProposalRow.jsx
+++ src/features/project/ProposalRow.jsx
@@ -1,19 +1,56 @@
 import Table from "../../ui/Table";
 import truncateText from "./../../utils/truncateText";
+import Modal from "../../ui/Modal";
 import { toPersianNumbersWithComma } from "./../../utils/toPersianNumbers";
+import { useState } from "react";
+import ChangeProposalStatus from "./ChangeProposalStatus";
+
+const statusStyle = [
+  {
+    label: "رد شده",
+    className: "badge--danger",
+  },
+  {
+    label: "در انتظار تایید",
+    className: "badge--secondary",
+  },
+  {
+    label: "تایید شده",
+    className: "badge--success",
+  },
+];
 
 function ProposalRow({ proposal, index }) {
+  const { status, user } = proposal;
+  const [open, setOpen] = useState(false);
+
   return (
     <Table.Row>
       <td>{index + 1}</td>
-      <td>{proposal.user.name}</td>
+      <td>{user.name}</td>
       <td>
         <p>{truncateText(proposal.description, 50)}</p>
       </td>
       <td>{proposal.duration} روز</td>
       <td>{toPersianNumbersWithComma(proposal.price)}</td>
-      <td>{proposal.status}</td>
-      <td>++</td>
+      <td>
+        <span className={`badge ${statusStyle[status].className}`}>
+          {statusStyle[status].label}
+        </span>
+      </td>
+      <td>
+        <Modal
+          title="تغییر وضعیت درخواست"
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          <ChangeProposalStatus
+            proposalId={proposal._id}
+            onClose={() => setOpen(false)}
+          />
+        </Modal>
+        <button onClick={() => setOpen(true)}>تغییر وضعیت</button>
+      </td>
     </Table.Row>
   );
 }
