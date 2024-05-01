ef0edaf7b821b559afb5bc41633d64a812c262b3
Hossein Salari <hosein.salari81@gmail.com>
2024-05-01 18:05:13 +0330

    implement dark mode on project

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -26,14 +26,14 @@ export default Sidebar;
 
 function CustomNavLink({ children, to }) {
   const navlinkClass =
-    "flex items-center gap-x-2 hover:bg-primary-100/50 hover:text-primary-900 px-2 py-1.5 rounded-lg transition-all duration-300";
+    "flex items-center gap-x-2 hover:bg-primary-100/80 hover:text-primary-900 px-2 py-1.5 rounded-lg transition-all duration-300";
 
   return (
     <NavLink
       to={to}
       className={({ isActive }) =>
         isActive
-          ? `${navlinkClass} bg-primary-100/50 text-primary-900`
+          ? `${navlinkClass} bg-primary-100/80 text-primary-900`
           : `${navlinkClass} text-secondary-600`
       }
     >
