afaa4a49819ac3de6de0ac851994e0043859c81e
Hossein Salari <hosein.salari81@gmail.com>
2024-05-03 00:33:50 +0330

    create useLocalStorageState hook

---
+++ src/hooks/useLocalStorageState.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function useLocalStorageState(key, initialState) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
