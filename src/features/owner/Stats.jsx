e4a203906b1be8c60a32b779c68a44524d744dda
Hossein Salari <hosein.salari81@gmail.com>
2024-05-04 23:54:34 +0330

    create Stat component

--- src/features/owner/Stats.jsx
+++ src/features/owner/Stats.jsx
@@ -1,3 +1,10 @@
+import {
+  HiCollection,
+  HiCurrencyDollar,
+  HiOutlineViewGrid,
+} from "react-icons/hi";
+import Stat from "./Stat";
+
 function Stats({ projects }) {
   const numOfProjects = projects.length;
   const numOfOpenProjects = projects.map((p) => p.status === "OPEN").length;
@@ -7,7 +14,28 @@ function Stats({ projects }) {
     0
   );
 
-  return <div>Stats</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <Stat
+        color="blue"
+        title="پروژه ها"
+        value={numOfProjects}
+        icon={<HiOutlineViewGrid className="w-20 h-20" />}
+      />
+      <Stat
+        color="green"
+        title="پروژه های باز"
+        value={numOfOpenProjects}
+        icon={<HiCurrencyDollar className="w-20 h-20" />}
+      />
+      <Stat
+        color="yellow"
+        title="درخواست ها"
+        value={numOfProposals}
+        icon={<HiCollection className="w-20 h-20" />}
+      />
+    </div>
+  );
 }
 
 export default Stats;
