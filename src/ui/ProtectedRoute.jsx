321a954382de8874cae87737b2722152790f5b47
Hossein Salari <hosein.salari81@gmail.com>
2024-05-17 16:11:43 +0330

    add useAuthorize hook

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import useAuthorize from "../features/authentication/useAuthorize";
+
+function ProtectedRoute({ children }) {
+  const { isLoading, isAuthenticated, isAuthorized } = useAuthorize();
+  console.log(isAuthorized);
+
+  return children;
+}
+
+export default ProtectedRoute;
