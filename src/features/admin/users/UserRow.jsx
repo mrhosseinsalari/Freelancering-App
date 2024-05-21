45b5de3c9a9d42e33f0f87ee653c6e6bce1dc243
Hossein Salari <hosein.salari81@gmail.com>
2024-05-21 16:04:28 +0330

    create users table in admin panel

---
+++ src/features/admin/users/UserRow.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import Modal from "../../../ui/Modal";
+import Table from "../../../ui/Table";
+
+const statusStyle = [
+  {
+    label: "رد شده",
+    className: "badge--danger",
+  },
+  {
+    label: "در انتظار تایید",
+    className: "badge--secondary",
+  },
+  {
+    label: "تایید شده",
+    className: "badge--success",
+  },
+];
+
+const userRoles = {
+  ADMIN: {
+    label: "ادمین",
+  },
+  OWNER: {
+    label: "کارفرما",
+  },
+  FREELANCER: {
+    label: "فریلنسر",
+  },
+};
+
+function UserRow({ user, index }) {
+  const [open, setOpen] = useState(false);
+  const { name, email, phoneNumber, role, status } = user;
+
+  return (
+    <Table.Row>
+      <td>{index + 1}</td>
+      <td>{name}</td>
+      <td>{email}</td>
+      <td>{phoneNumber}</td>
+      <td>{userRoles[role].label}</td>
+      <td>
+        <span className={`badge ${statusStyle[status].className}`}>
+          {statusStyle[status].label}
+        </span>
+      </td>
+      <td>
+        <Modal
+          title="تغییر وضعیت کاربر"
+          open={open}
+          onClose={() => setOpen(false)}
+        ></Modal>
+        <button onClick={() => setOpen(true)}>تغییر وضعیت</button>
+      </td>
+    </Table.Row>
+  );
+}
+
+export default UserRow;
